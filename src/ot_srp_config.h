b2479fcb1148e8d41e9f98222c5e8e334826e166
yanntalhouarne <talhouarne.yann@gmail.com>
2023-10-26 15:52:44 -0500

    SRP_CLIENT_UNIQUE = 1

--- src/ot_srp_config.h
+++ src/ot_srp_config.h
@@ -1,6 +1,6 @@
 // un-comment macro below to append a random number to the SRP service hostname 
 //#define SRP_CLIENT_RNG 1
-//#define SRP_CLIENT_UNIQUE 1
+#define SRP_CLIENT_UNIQUE 1
 
 #define SRP_CLIENT_HOSTNAME "nrf52840dg"
 #define SRP_CLIENT_SERVICE_INSTANCE "nrf52840dg"
